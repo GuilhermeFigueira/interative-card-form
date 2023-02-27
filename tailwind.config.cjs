81ba451de353e4c433fe66c5342213f9d88c76b3
Guilherme Figueira <guibf2004@gmail.com>
2023-02-26 21:08:17 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.tsx", "./index.html"],
+	theme: {
+		fontFamily: { sans: ["Space Grotesk", "sans-serif"] },
+		extend: {
+			colors: {
+				white: "hsl(0, 0%, 100%)",
+				violet: {
+					light: "hsl(270, 3%, 87%)",
+					gray: " hsl(279, 6%, 55%)",
+					dark: "hsl(278, 68%, 11%)",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
