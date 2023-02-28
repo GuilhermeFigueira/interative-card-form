aafe9070109e8147c616f76ccd74ce269f6fd443
Guilherme Figueira <guibf2004@gmail.com>
2023-02-28 11:57:43 -0300

    início dos cards

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,25 @@ function App() {
 	const [expirationDate, setExpirationDate] = useState("00/00");
 
 	return (
-		<div className="h-screen grid grid-rows-[30%_1fr] sm:grid-cols-[30%_1fr] sm:grid-rows-none">
-			<header className="bg-background_mobile sm:bg-background_desktop bg-cover bg-no-repeat"></header>
-			<main>{isConfirmed ? <ThankYouPage /> : <CardForm />}</main>
+		<div>
+			<div className="absolute">
+				<div>
+					<img src="/bg-card-back.png" alt="Back of the card" />
+				</div>
+				<div className="z-10 bg-front_card flex flex-col items-center">
+					<div></div>
+					<div className="flex flex-col">
+						<div>0000 0000 0000 0000</div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+			<div className="h-screen w-screen grid grid-rows-[30%_1fr] sm:grid-cols-[30%_1fr] sm:grid-rows-none">
+				<header className="bg-background_mobile sm:bg-background_desktop bg-cover bg-no-repeat"></header>
+				<main className="flex h-full m-auto items-center">
+					{isConfirmed ? <ThankYouPage /> : <CardForm />}
+				</main>
+			</div>
 		</div>
 	);
 }
