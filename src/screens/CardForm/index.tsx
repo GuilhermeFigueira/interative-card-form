16a1c0d21a22b20d7c82cd56fb3966760673de4c
Guilherme Figueira <guibf2004@gmail.com>
2023-03-03 11:35:44 -0300

    form validation

--- src/screens/CardForm/index.tsx
+++ src/screens/CardForm/index.tsx
@@ -8,11 +8,24 @@ export default function CardForm() {
 		event.preventDefault();
 		setIsConfirmed(true);
 	};
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			name: "",
+			number: "",
+			month: {},
+			year: {},
+			cvc: {},
+		},
+	});
 
 	return (
 		<div>
 			<form
-				onSubmit={sendForm}
+				onSubmit={handleSubmit((data) => console.log(data))}
 				className="flex flex-col p-5 sm:p-16 gap-5 max-w-screen-sm"
 			>
 				<div className="flex flex-col">
@@ -20,24 +33,43 @@ export default function CardForm() {
 					<div className="input_gradient">
 						<input
 							type="text"
-							name="name"
+							{...register("name", {
+								required: "Can't be blank",
+								minLength: {
+									value: 3,
+									message: "The minimum lenght is 3",
+								},
+								maxLength: {
+									value: 60,
+									message: "The max lenght is 60",
+								},
+							})}
 							placeholder="e.g Jane Appleseed"
 							className="w-full"
 						/>
 					</div>
+					<span>{errors.name?.message}</span>
 				</div>
 				<div className="flex flex-col">
 					<label htmlFor="number">CARD NUMBER</label>
 					<div className="input_gradient">
 						<input
 							type="text"
-							name="number"
+							{...register("number", {
+								required: "Can't be blank",
+								minLength: {
+									value: 16,
+									message: "The minimum lenght is 16",
+								},
+								maxLength: {
+									value: 16,
+									message: "The max lenght is 16",
+								},
+							})}
 							placeholder="e.g 1234 5678 9123 0000"
-							onChange={(event) => {
-								setCardNumbers(event.target.value);
-							}}
 						/>
 					</div>
+					<span>{errors.number?.message}</span>
 				</div>
 
 				<div className="grid grid-cols-[40%_1fr] gap-3">
@@ -47,17 +79,48 @@ export default function CardForm() {
 							<div className="input_gradient">
 								<input
 									type="number"
-									name="month"
+									{...register("month", {
+										required: "Can't be blank",
+										minLength: {
+											value: 2,
+											message:
+												"Please enter a valid month",
+										},
+										maxLength: {
+											value: 2,
+											message:
+												"Please enter a valid month",
+										},
+										max: {
+											value: 12,
+											message:
+												"Please enter a valid month",
+										},
+									})}
 									placeholder="MM"
 								/>
 							</div>
 							<div className="input_gradient">
 								<input
 									type="number"
-									name="year"
+									{...register("year", {
+										required: "Can't be blank",
+										minLength: {
+											value: 2,
+											message:
+												"Please enter a valid year number ",
+										},
+										maxLength: {
+											value: 2,
+											message:
+												"Please enter a valid year number",
+										},
+									})}
 									placeholder="YY"
 								/>
 							</div>
+							<span>{errors.month?.message}</span>
+							<span>{errors.year?.message}</span>
 						</div>
 					</div>
 					<div className="flex flex-col">
@@ -65,10 +128,25 @@ export default function CardForm() {
 						<div className="input_gradient">
 							<input
 								type="number"
-								name="cvc"
+								{...register("cvc", {
+									required: "Can't be blank",
+									minLength: {
+										value: 3,
+										message: "Please enter a valid CVC",
+									},
+									maxLength: {
+										value: 3,
+										message: "Please enter a valid CVC",
+									},
+									max: {
+										value: 999,
+										message: "Please enter a valid CVC",
+									},
+								})}
 								placeholder="e.g 123"
 							/>
 						</div>
+						<span>{errors.cvc?.message}</span>
 					</div>
 				</div>
 				<button className="bg-violet-dark text-white rounded-md py-4 mt-2">
